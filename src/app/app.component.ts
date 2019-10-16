787f361ed407ba17fff0d82caf28342cd7990c5e
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-10-16 17:38:16 -0300

    Socket.IO Chat - CLIENT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  ViewChildren,
+  QueryList
+} from '@angular/core';
+import { SocketIoService } from './socket-io.service';
+import { Message } from './message';
+import { Subscription } from 'rxjs';
+import { MatList, MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  nickname: string;
+  message: string;
+  messages: Message[] = [];
+  private subscriptionMessages: Subscription;
+  private subscriptionList: Subscription;
+
+  @ViewChild(MatList, { read: ElementRef, static: true }) list: ElementRef;
+  @ViewChildren(MatListItem) listItems: QueryList<MatListItem>;
+
+  constructor(private socketService: SocketIoService) {}
+
+  ngOnInit() {
+    this.subscriptionMessages = this.socketService
+      .messages()
+      .subscribe((m: Message) => {
+        console.log(m);
+        this.messages.push(m);
+      });
+  }
+
+  ngAfterViewInit() {
+    this.subscriptionList = this.listItems.changes.subscribe(e => {
+      this.list.nativeElement.scrollTop = this.list.nativeElement.scrollHeight;
+    });
+  }
+
+  send() {
+    this.socketService.send({
+      from: this.nickname,
+      message: this.message
+    });
+    this.message = '';
+  }
+
+  ngOnDestroy() {
+    this.subscriptionMessages.unsubscribe();
+    this.subscriptionList.unsubscribe();
+  }
 }
